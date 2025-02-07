6d55c319bdd8d718239eb8647c43dab12a5eb481
dorichapi <38826817+dorichapi@users.noreply.github.com>
2025-02-07 20:30:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ✅ Google Apps ScriptのURLをここに貼り付け
-const apiUrl = "https://script.google.com/macros/s/AKfycby4XwDzmcDdVy0odJXbZpf3FApO_99SWYc0Wtg9fvisnz2tQYbGBGZzO6FR9piLAHgk/exec";
+const apiUrl = "https://script.google.com/macros/s/AKfycbyEeabu03SajljS3kwDVUqWgWku2jVVQgULGFbDQxG8W31V6RnV6IlMryZuLI1ciWR0/exec";
 
 // ✅ データ取得 & グラフ表示
 async function fetchData() {
